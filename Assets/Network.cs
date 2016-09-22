0484cf963ea17a99daac6a8369d2cce5c12588be
Kevin Hurley <kphurley@gmail.com>
2016-09-22 14:06:08 -0500

    disconnection works

--- Assets/Network.cs
+++ Assets/Network.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 using SocketIO;
 
 public class Network : MonoBehaviour {
@@ -8,12 +8,17 @@ public class Network : MonoBehaviour {
 
 	public GameObject player;
 
+	Dictionary<string, GameObject> players;
+
 	void Start () {
 		socket = GetComponent<SocketIOComponent> ();
 
+		players = new Dictionary<string, GameObject> ();
+
 		//Event handler registration
 		socket.On("open", OnConnected);
-		socket.On ("spawn", OnSpawn);
+		socket.On("spawn", OnSpawn);
+		socket.On ("disconnected", OnDisconnected); 
 	}
 
 	void OnConnected(SocketIOEvent e){
@@ -22,8 +27,17 @@ public class Network : MonoBehaviour {
 
 	void OnSpawn(SocketIOEvent e) {
 		
-		Debug.Log ("Spawning player: " + e.data);
-		Instantiate (player);
+		Debug.Log ("Spawning player: " + e.data.ToString());
+		var newPlayer = Instantiate (player);
+		players.Add (e.data ["id"].ToString (), newPlayer);
+		Debug.Log ("Player count: " + players.Count);
+	}
+
+	void OnDisconnected(SocketIOEvent e) {
+		var id = e.data ["id"].ToString ();
+		Debug.Log ("Player disconnected: " + id);
+		Destroy (players [id]);
+		players.Remove (id);
 	}
 
 }
