721a8000df61861cdd34c4903cc15424b34d4bb4
Kevin Hurley <kphurley@gmail.com>
2016-09-23 00:31:41 -0500

    Working but buggy

---
+++ Assets/MovementEmitter.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using SocketIO;
+
+public class MovementEmitter : MonoBehaviour {
+
+	private Rigidbody rb;
+	public SocketIOComponent socket;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate () {
+		//Debug.Log (getMovementJSON());
+		socket.Emit("move", getMovementJSON());
+	}
+
+
+	string vectorToObj(Vector3 vector, string desc){
+		
+//		return "{\"x\":\"" + vector.x + "\"," +
+//			"\"y\":\"" + vector.y + "\"," +
+//			"\"z\":\"" + vector.z + "\"}";
+
+		return "\"x" + desc + "\":\"" + vector.x + "\"," +
+			   "\"y" + desc + "\":\"" + vector.y + "\"," +
+			   "\"z" + desc + "\":\"" + vector.z + "\"";
+
+	}
+
+	JSONObject getMovementJSON() {
+		//return new JSONObject("{ \"position\":" + vectorToObj (transform.position) + "," +
+		//	"\"velocity\":" + vectorToObj (rb.velocity) + "}");
+		return new JSONObject("{" + vectorToObj (transform.position, "Pos") + "," +
+			vectorToObj (rb.velocity, "Vel") + "}");
+	}
+}
---
+++ Assets/NetworkMove.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using SocketIO;
+
+
+public class NetworkMove : MonoBehaviour {
+
+	public SocketIOComponent socket;
+	public string id;
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		id = gameObject.name;
+	}
+
+	void AssignNetwork(Network n){
+		socket = n.GetComponent<SocketIOComponent> ();
+	}
+
+	void ApplyNetworkMove(string posValues){
+		string[] values = posValues.Split (',');
+
+		//applying the components in this way works but doesnt turn the car!
+		transform.position = new Vector3 (float.Parse (values [1]),
+			float.Parse (values [3]), float.Parse (values [5]));
+		rb.velocity = new Vector3 (float.Parse (values [7]),
+			float.Parse (values [9]), float.Parse (values [11]));
+	}
+		
+
+}
+	
+
